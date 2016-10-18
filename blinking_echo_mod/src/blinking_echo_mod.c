3fca47ce4aab12d2e85665aa927e5ba51acd72f6
ffsuarez <fmsuarez92@gmail.com>
2016-10-18 18:39:46 -0300

    comenzado otro intento

--- blinking_echo_mod/src/blinking_echo_mod.c
+++ blinking_echo_mod/src/blinking_echo_mod.c
@@ -72,7 +72,7 @@
 #include "ciaaPOSIX_string.h" /* <= string header */
 #include "ciaak.h"            /* <= ciaa kernel header */
 #include "blinking_echo_mod.h"         /* <= own header */
-#include "chip.h"
+
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data declaration]==============================*/
@@ -112,29 +112,6 @@ static uint32_t Periodic_Task_Counter;
 /*==================[external data definition]===============================*/
 
 /*==================[internal functions definition]==========================*/
-char* itoa(int value, char* result, int base) {
-   // check that the base if valid
-   if (base < 2 || base > 36) { *result = '\0'; return result; }
-
-   char* ptr = result, *ptr1 = result, tmp_char;
-   int tmp_value;
-
-   do {
-      tmp_value = value;
-      value /= base;
-      *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-   } while ( value );
-
-   // Apply negative sign
-   if (tmp_value < 0) *ptr++ = '-';
-   *ptr-- = '\0';
-   while(ptr1 < ptr) {
-      tmp_char = *ptr;
-      *ptr--= *ptr1;
-      *ptr1++ = tmp_char;
-   }
-   return result;
-}
 
 /*==================[external functions definition]==========================*/
 /** \brief Main function
@@ -295,13 +272,7 @@ TASK(PeriodicTask)
    /* Print Task info */
    Periodic_Task_Counter++;
    ciaaPOSIX_printf("Periodic Task: %d\n", Periodic_Task_Counter);
-   //AGREGADO
 
-   Chip_UART_SendByte(LPC_USART2,'a'-32);
-
-
-
-   //TERMINA LO AGREGADO
    /* end PeriodicTask */
    TerminateTask();
 }
