9e8d6797907b0afd0d6f67c2e7bc1035ab290bbd
ffsuarez <fmsuarez92@gmail.com>
2016-11-08 22:53:56 -0300

    modificado ejemplo osek_sapi_uart

--- osek_sapi_uart/src/osek_sapi_uart.c
+++ osek_sapi_uart/src/osek_sapi_uart.c
@@ -35,11 +35,8 @@
 
 /** \brief Blinking_echo example source file
  **
- ** This is a mini example of the CIAA Firmware to test the periodical
- ** task excecution and serial port funcionality.
- ** To run this sample in x86 plataform you must enable the funcionality of
- ** uart device setting a value of une or more of folowing macros defined
- ** in header file modules/plataforms/x86/inc/ciaaDriverUart_Internal.h
+ ** This is a mini example of the CIAA Firmware.
+ **
  **/
 
 /** \addtogroup CIAA_Firmware CIAA Firmware
@@ -56,23 +53,32 @@
  * PR           Pablo Ridolfi
  * JuCe         Juan Cecconi
  * GMuro        Gustavo Muro
+ * ErPe         Eric Pernia
  */
 
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
+ * 20150603 v0.0.3   ErPe change uint8 type by uint8_t
+ *                        in line 172
  * 20141019 v0.0.2   JuCe add printf in each task,
  *                        remove trailing spaces
  * 20140731 v0.0.1   PR   first functional version
  */
+/*
+ * https://groups.google.com/forum/#!searchin/ciaa-firmware/boolean%7Csort:relevance/ciaa-firmware/SNLmTVZbIkk/iSPYr-l-EAAJ
+ * DICE QUE En concreto, el problema con la nueva lpcopen es que define TRUE, FALSE y boolean de forma que chocan con las definiciones del RTOS.
 
+	Hay tres soluciones.
+
+*UNA DE ELLAS ES Eliminar las definiciones de TRUE/FALSE/boolean de lpcopen (no muy limpio pero aceptable)
+*/
 /*==================[inclusions]=============================================*/
 #include "os.h"               /* <= operating system header */
-#include "ciaaPOSIX_stdio.h"  /* <= device handler header */
-#include "ciaaPOSIX_string.h" /* <= string header */
-#include "ciaak.h"            /* <= ciaa kernel header */
 #include "osek_sapi_uart.h"         /* <= own header */
-
+#include "sAPI.h"
+//#include "sAPI_Uart.h"
+//#include "Flex_LCD420_CIAA.h"
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data declaration]==============================*/
@@ -80,35 +86,13 @@
 /*==================[internal functions declaration]=========================*/
 
 /*==================[internal data definition]===============================*/
-/** \brief File descriptor for digital input ports
- *
- * Device path /dev/dio/in/0
- */
-static int32_t fd_in;
 
 /** \brief File descriptor for digital output ports
  *
  * Device path /dev/dio/out/0
  */
 static int32_t fd_out;
-
-/** \brief File descriptor of the USB uart
- *
- * Device path /dev/serial/uart/1
- */
-static int32_t fd_uart1;
-
-/** \brief File descriptor of the RS232 uart
- *
- * Device path /dev/serial/uart/2
- */
-static int32_t fd_uart2;
-
-/** \brief Periodic Task Counter
- *
- */
-static uint32_t Periodic_Task_Counter;
-
+uint8_t data;
 /*==================[external data definition]===============================*/
 
 /*==================[internal functions definition]==========================*/
@@ -125,9 +109,8 @@ static uint32_t Periodic_Task_Counter;
  */
 int main(void)
 {
-   /* Starts the operating system in the Application Mode 1 */
-   /* This example has only one Application Mode */
-   StartOS(AppMode1);
+	 //boardConfig();
+	StartOS(AppMode1);
 
    /* StartOs shall never returns, but to avoid compiler warnings or errors
     * 0 is returned */
@@ -163,122 +146,168 @@ void ErrorHook(void)
  *
  * This task is started automatically in the application mode 1.
  */
-TASK(InitTask)
+TASK(INICIAL)
 {
-   /* init CIAA kernel and devices */
-   ciaak_start();
+	digitalConfig( 0, ENABLE_DIGITAL_IO );
+	digitalConfig( TEC1, INPUT );
+    digitalConfig( TEC2, INPUT );
+    digitalConfig( TEC3, INPUT );
+    digitalConfig( TEC4, INPUT );
+    digitalConfig( LEDR, OUTPUT );
+    digitalConfig( LEDG, OUTPUT );
+    digitalConfig( LEDB, OUTPUT );
+    digitalConfig( LED1, OUTPUT );
+    digitalConfig( LED2, OUTPUT );
+    digitalConfig( LED3, OUTPUT );
+    lcd_init();
+    lcd_gotoxy(1,1);
+    uartConfig( UART_USB, 115200 );
+	ActivateTask(uart);
+	SetEvent(uart,recibo);
+	TerminateTask();
 
-   ciaaPOSIX_printf("Init Task...\n");
-   /* open CIAA digital inputs */
-   fd_in = ciaaPOSIX_open("/dev/dio/in/0", ciaaPOSIX_O_RDONLY);
 
-   /* open CIAA digital outputs */
-   fd_out = ciaaPOSIX_open("/dev/dio/out/0", ciaaPOSIX_O_RDWR);
+    //digitalConfig( DIO15, OUTPUT );
+   /* activate periodic task:
+    *  - for the first time after 350 ticks (350 ms)
+    *  - and then every 250 ticks (250 ms)
+    */
+   //SetRelAlarm(ActivatePERIODICA2, 350, 250);
 
-   /* open UART connected to USB bridge (FT2232) */
-   fd_uart1 = ciaaPOSIX_open("/dev/serial/uart/1", ciaaPOSIX_O_RDWR);
-
-   /* open UART connected to RS232 connector */
-   fd_uart2 = ciaaPOSIX_open("/dev/serial/uart/2", ciaaPOSIX_O_RDWR);
-
-   /* change baud rate for uart usb */
-   ciaaPOSIX_ioctl(fd_uart1, ciaaPOSIX_IOCTL_SET_BAUDRATE, (void *)ciaaBAUDRATE_115200);
-
-   /* change FIFO TRIGGER LEVEL for uart usb */
-   ciaaPOSIX_ioctl(fd_uart1, ciaaPOSIX_IOCTL_SET_FIFO_TRIGGER_LEVEL, (void *)ciaaFIFO_TRIGGER_LEVEL3);
-
-   /* activate example tasks */
-   Periodic_Task_Counter = 0;
-   SetRelAlarm(ActivatePeriodicTask, 200, 200);
-
-   /* Activates the SerialEchoTask task */
-   ActivateTask(SerialEchoTask);
-
-   /* end InitTask */
-   TerminateTask();
-}
-
-/** \brief Serial Echo Task
- *
- * This tasks waits for input data from fd_uart1 and writes the received data
- * to fd_uart1 and fd_uart2. This taks alos blinkgs the output 5.
- *
- */
-TASK(SerialEchoTask)
-{
-   int8_t buf[20];   /* buffer for uart operation              */
-   uint8_t outputs;  /* to store outputs status                */
-   int32_t ret;      /* return value variable for posix calls  */
-
-   ciaaPOSIX_printf("SerialEchoTask...\n");
-   /* send a message to the world :) */
-   char message[] = "Hi! :)\nSerialEchoTask: Waiting for characters...\n";
-   ciaaPOSIX_write(fd_uart1, message, ciaaPOSIX_strlen(message));
-
-   while(1)
-   {
-      /* wait for any character ... */
-      ret = ciaaPOSIX_read(fd_uart1, buf, 20);
-
-      if(ret > 0)
-      {
-         /* ... and write them to the same device */
-         ciaaPOSIX_write(fd_uart1, buf, ret);
-
-         /* also write them to the other device */
-         ciaaPOSIX_write(fd_uart2, buf, ret);
-      }
-
-      /* blink output 5 with each loop */
-      ciaaPOSIX_read(fd_out, &outputs, 1);
-      outputs ^= 0x20;
-      ciaaPOSIX_write(fd_out, &outputs, 1);
-   }
+   /* terminate task */
+   ChainTask(INICIAL);
 }
 
 /** \brief Periodic Task
  *
- * This task is activated by the Alarm ActivatePeriodicTask.
- * This task copies the status of the inputs bits 0..3 to the output bits 0..3.
- * This task also blinks the output 4
+ * This task is started automatically every time that the alarm
+ * ActivatePeriodicTask expires.
+ *
  */
-TASK(PeriodicTask)
+TASK(LED_1)
 {
-   /*
-    * Example:
-    *    Read inputs 0..3, update outputs 0..3.
-    *    Blink output 4
-    */
+	WaitEvent(tecla1);
+	digitalWrite(LED1,ON);
+	digitalWrite(LED2,OFF);
+	digitalWrite(LED3,OFF);
+	digitalWrite(LEDB,OFF);
+	digitalWrite(LEDR,OFF);
+	digitalWrite(LEDG,OFF);
+	ClearEvent(tecla1);
+	TerminateTask();
+}
 
-   /* variables to store input/output status */
-   uint8_t inputs = 0, outputs = 0;
+TASK(LED_2)
+{
+	WaitEvent(tecla2);
+	digitalWrite(LED1,OFF);
+	digitalWrite(LED2,ON);
+	digitalWrite(LED3,OFF);
+	digitalWrite(LEDB,OFF);
+	digitalWrite(LEDR,OFF);
+	digitalWrite(LEDG,OFF);
+	ClearEvent(tecla2);
+	TerminateTask();
 
-   /* read inputs */
-   ciaaPOSIX_read(fd_in, &inputs, 1);
+}
 
-   /* read outputs */
-   ciaaPOSIX_read(fd_out, &outputs, 1);
+TASK(LED_3)
+{
+	WaitEvent(tecla3);
+	digitalWrite(LED1,OFF);
+	digitalWrite(LED2,OFF);
+	digitalWrite(LED3,ON);
+	digitalWrite(LEDB,OFF);
+	digitalWrite(LEDR,OFF);
+	digitalWrite(LEDG,OFF);
+	ClearEvent(tecla3);
+	TerminateTask();
+}
 
-   /* update outputs with inputs */
-   outputs &= 0xF0;
-   outputs |= inputs & 0x0F;
+TASK(LED_B)
+{
+	WaitEvent(tecla1);
+	digitalWrite(LED1,OFF);
+	digitalWrite(LED2,OFF);
+	digitalWrite(LED3,OFF);
+	digitalWrite(LEDB,ON);
+	digitalWrite(LEDR,OFF);
+	digitalWrite(LEDG,OFF);
+	ClearEvent(teclaB);
+	TerminateTask();
+}
+TASK(LED_R)
+{
+	WaitEvent(teclaR);
+	digitalWrite(LED1,OFF);
+	digitalWrite(LED2,OFF);
+	digitalWrite(LED3,OFF);
+	digitalWrite(LEDB,OFF);
+	digitalWrite(LEDR,ON);
+	digitalWrite(LEDG,OFF);
+	ClearEvent(teclaR);
+	TerminateTask();
+}
 
-   /* blink */
-   outputs ^= 0x10;
+TASK(LED_G)
+{
+	WaitEvent(teclaG);
+	digitalWrite(LED1,OFF);
+	digitalWrite(LED2,OFF);
+	digitalWrite(LED3,OFF);
+	digitalWrite(LEDB,OFF);
+	digitalWrite(LEDR,OFF);
+	digitalWrite(LEDG,ON);
+	ClearEvent(teclaG);
+	TerminateTask();
+}
 
-   /* write */
-   ciaaPOSIX_write(fd_out, &outputs, 1);
+TASK(uart){
+	WaitEvent(recibo);
+	ClearEvent(recibo);
+	if(data=='LED1'){
+		ActivateTask(LED_1);
+		SetEvent(LED_1,tecla1);
+		uartWrite("UNO\n");
+		TerminateTask();
+	}
+	if(data=='LED2'){
+		ActivateTask(LED_2);
+		SetEvent(LED_2,tecla2);
+		uartWrite("DOS\n");
+	}
+	if(data=='LED3'){
+		ActivateTask(LED_3);
+		SetEvent(LED_2,tecla3);
+		uartWrite("TRES\n");
+	}
+	if(data=='LEDB'){
+		ActivateTask(LED_B);
+		SetEvent(LED_B,teclaB);
+		uartWrite("LEDB\n");
+	}
+	if(data=='LEDR'){
+		ActivateTask(LED_R);
+		SetEvent(LED_R,teclaR);
+		uartWrite("LEDR\n");
+	}
+	if(data=='LEDG'){
+		ActivateTask(LED_G);
+		SetEvent(LED_G,teclaG);
+		uartWrite("LEDG\n");
+	}
 
-   /* Print Task info */
-   Periodic_Task_Counter++;
-   ciaaPOSIX_printf("Periodic Task: %d\n", Periodic_Task_Counter);
+	TerminateTask();
+}
 
-   /* end PeriodicTask */
-   TerminateTask();
+ISR(UART2_IRQHandler)
+{
+	data = uartReadByte( UART_USB );
+	ActivateTask(uart);
+	SetEvent(uart,recibo);
 }
 
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /*==================[end of file]============================================*/
-
